cae4c4797626deaeec70552f0e5efc50ab4e0114
AdityaJumde <adityajumde@gmail.com>
2015-10-26 13:02:48 -0400

    Unit Testing and Encryption Part

--- CypherBox.Tests/UnitTest1.cs
+++ CypherBox.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using encryptionBusinessLayer;
 
 namespace CypherBox.Tests
 {
@@ -7,8 +8,23 @@ namespace CypherBox.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestEncryption()
         {
+            string testString = "I like it this way";
+            string expected = "IdS/A5GPERGHQ5CbEZSrEdCzAdKDQpGnApG3";
+            clsXor BizEncryDecry = new clsXor();
+            string Actual=BizEncryDecry.encryption(testString,"hidden");
+            StringAssert.Contains(expected,Actual);
+        }
+
+        [TestMethod]
+        public void TestDecryption()
+        {
+            string testString = "IdS/A5GPERGHQ5CbEZSrEdCzAdKDQpGnApG3";
+            string expected = "I like it this way";
+            clsXor BizEncryDecry = new clsXor();
+            string Actual = BizEncryDecry.decryption(testString, "hidden");
+            StringAssert.Contains(expected, Actual);
         }
     }
 }
